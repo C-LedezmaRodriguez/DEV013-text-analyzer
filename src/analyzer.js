4ffd7b8613a623956efde288c16165ac7073d316
Camila Ledezma <camilaledezmar@gmail.com>
2024-01-10 11:34:44 -0400

    Text analyzer finished

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,63 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const cont = text.trim().split(" ");
+    return cont.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const signos = text.replace(/[^\w]/g, "");
+    // let count = 0;
+    // for (let i = 0; i <= text.length; i++) {
+    //   if (text[i] !== " " && text[i] !== (/[^\w]/g)) {
+    //     count = count + 1;
+    //   }
+    // }
+    // return count;
+    return signos.length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    // const a = text.replace(/[^\w]/g,"");
+    let a = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] !== " "  && text[i] !== (/[^\w]/g)) {
+        a = a + 1;
+      }
+    }
+    const b = text.trim().split((" "));
+    const long = (a / b.length).toFixed(2);
+    return parseFloat(long);
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    // const numCo = text.replace(/[^0-9]/g, "");
+    // return numCo.length;
+    let numCo = 0;
+    const num = text.trim().split(" ");
+    for (let i = 0; i < num.length; i++) {
+      const numero = parseFloat(num[i] * 1);
+      if (!isNaN(numero)) {
+        numCo = numCo + 1;
+      }
+    }
+    return numCo;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let numSum = 0;
+    const num = text.trim().split(" ");
+    for (let i = 0; i < num.length; i++) {
+      const numero = parseFloat(num[i] * 1);
+      if (!isNaN(numero) && num[i] !== " ") {
+        numSum += numero;
+      }
+    }
+    return numSum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer
+const btnreset = document.getElementById("reset-button");
+const texto = document.querySelector('textarea[name="user-input"]');
+btnreset.addEventListener("click", function reset() {
+  texto.value = "";
+  document.querySelector("li[data-testid='word-count']").innerHTML =
+    "Word-count: 0";
+  document.querySelector("li[data-testid='character-count']").innerHTML =
+    "Character-count: 0";
+  document.querySelector(
+    "li[data-testid='character-no-spaces-count']"
+  ).innerHTML = "Character-no-spaces-count: 0";
+  document.querySelector("li[data-testid='number-count']").innerHTML =
+    "Number-count: 0";
+  document.querySelector("li[data-testid='number-sum']").innerHTML =
+    "Number-sum: 0";
+  document.querySelector("li[data-testid='word-length-average']").innerHTML =
+    "Word-length-average: 0";
+});
+// document.querySelector('form').reset();
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//escuchador cuando llenan el textarea
+texto.addEventListener("input", function () {
+  const wordCount = analyzer.getWordCount(texto.value);
+  const wc = document.querySelector("li[data-testid='word-count']");
+  wc.textContent = "Word-count: " + wordCount;
+
+  const characterCount = analyzer.getCharacterCount(texto.value);
+  const cc = document.querySelector("li[data-testid='character-count']");
+  cc.textContent = "Character-count: " + characterCount;
+
+  const charNoSpaces = analyzer.getCharacterCountExcludingSpaces(texto.value);
+  const cNe = document.querySelector(
+    "li[data-testid='character-no-spaces-count']"
+  );
+  cNe.textContent = "Character-no-spaces-count: " + charNoSpaces;
+
+  const numberCount = analyzer.getNumberCount(texto.value);
+  const nc = document.querySelector("li[data-testid='number-count']");
+  nc.textContent = "Number-count: " + numberCount;
+
+  const numberSum = analyzer.getNumberSum(texto.value);
+  const ns = document.querySelector("li[data-testid='number-sum']");
+  ns.textContent = "Number-sum: " + numberSum;
+
+  const wordLength = analyzer.getAverageWordLength(texto.value);
+  const wl = document.querySelector("li[data-testid='word-length-average']");
+  wl.textContent = "Word-length-average: " + wordLength;
+});
